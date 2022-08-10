03711cb8631af5a4c3106392c9d9bba809081555
marcelkawski <marcel.kawski1@gmail.com>
2022-08-10 14:24:39 +0200

    Add script cleaning faces filenames from face number

---
+++ data_prep/clean_faces_names.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import re
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from config import FACES_DIR
+from data_prep.utils import get_dir_content, get_all_filenames
+
+
+def rename_file(path, old_name, new_name):
+    os.rename(os.path.abspath(os.path.join(os.sep, path, old_name)),
+              os.path.abspath(os.path.join(os.sep, path, new_name)))
+
+
+if __name__ == '__main__':
+    if not os.path.exists(FACES_DIR) or not os.listdir(FACES_DIR):
+        print('Faces directory does not exist or is empty so there is nothing to clean.\n')
+        sys.exit()
+
+    faces_dirs = get_dir_content(FACES_DIR)
+
+    for face_dirname in faces_dirs:
+        face_dir = os.path.abspath(os.path.join(os.sep, FACES_DIR, face_dirname))
+        faces_files_to_clean = [filename for filename in get_all_filenames(face_dir) if 'face' in filename]
+        for f in faces_files_to_clean:
+            _, ext = os.path.splitext(f)
+            rename_file(face_dir, f, re.sub(rf'_face\d{ext}', '', f))
